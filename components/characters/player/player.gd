7b0643aa9a5609831582a79687b1b1c5c4697f2e
Nicolas Anelli <nickanelli@hotmail.com>
2024-11-06 02:06:45 -0300

    feat: replace character for new one

--- components/characters/player/player.gd
+++ components/characters/player/player.gd
@@ -1,4 +1,4 @@
-class_name PlayerOld
+class_name Player
 extends CharacterBody2D
 
 
@@ -6,30 +6,13 @@ signal player_leveled
 signal player_died
 
 
-@onready var _happy_boo: HappyBoo = %HappyBoo
 @onready var _health_component: HealthComponent = %HealthComponent
 @onready var _experience_component: ExperienceComponent = %ExperienceComponent
-
-
-const SPEED = 600
 
 
 func _ready() -> void:
 	_health_component.health_depleted.connect(_on_health_depleted)
 	_experience_component.leveled_up.connect(_on_leveled_up)
-
-
-
-func _physics_process(_delta: float) -> void:
-	var direction = PlayerInput.get_movement()
-	
-	velocity = direction * SPEED * GlobalTimer.get_factor()
-	move_and_slide()
-	
-	if velocity.length() > 0.0:
-		_happy_boo.play_walk_animation()
-	else:
-		_happy_boo.play_idle_animation()
 
 
 func _on_health_depleted() -> void:
