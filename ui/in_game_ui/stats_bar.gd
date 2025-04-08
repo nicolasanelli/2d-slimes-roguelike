897a92aa1256c88f6f049f3076e114da25e03d97
Nicolas Anelli <nickanelli@hotmail.com>
2025-04-07 23:45:55 -0300

    refactor: unifying things that belongs together

--- ui/in_game_ui/stats_bar.gd
+++ ui/in_game_ui/stats_bar.gd
@@ -1,19 +1,15 @@
 extends HBoxContainer
 
-
-var _stat_item_component = preload("res://ui/stat_item/stat_item.tscn")
-
+const _stat_item_component = preload("res://ui/in_game_ui/stat_item/stat_item.tscn")
 
 func _ready() -> void:
 	_clear()
 	CommandDispatcher.inventory_updated.connect(_on_inventory_updated)
 
-
 func _clear() -> void:
 	var list = get_children()
 	for n in range(list.size()):
 		list[n].queue_free()
-
 
 func _on_inventory_updated(inventary: Dictionary[String, BaseWeapon]) -> void:
 	_clear()
@@ -21,7 +17,6 @@ func _on_inventory_updated(inventary: Dictionary[String, BaseWeapon]) -> void:
 	for n in range(inventary.size()):
 		var weapon = (inventary.get(keys[n]) as BaseWeapon)
 		_add_item_stat(keys[n], weapon.get_resource())
-
 
 func _add_item_stat(_key: String, resource: BaseWeaponData) -> void:
 	var item: StatItem = _stat_item_component.instantiate()
